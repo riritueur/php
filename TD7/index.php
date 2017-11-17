40e223f6fd5a3afd393a2bf80b366923d6085b2f
Francois Rigaut <francois.rigaut@etu.unice.fr>
2017-11-17 14:29:19 +0100

    1.1 TD7

--- TD7/index.php
+++ TD7/index.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+
+<html lang="fr">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
   </head>
   <body>
-    <h1><a href="TD1/"><img src="./Entypo+/Entypo+/arrow-bold-right.svg" height="45px" width="65px"/>TD1</a></h1>
-    <h1><a href="TD2/"><img src="./Entypo+/Entypo+/arrow-bold-right.svg" height="45px" width="65px"/>TD2</a></h1>
-    <h1><a href="TD3/"><img src="./Entypo+/Entypo+/arrow-bold-right.svg" height="45px" width="65px"/>TD3</a></h1>
-    <h1><a href="TD4/"><img src="./Entypo+/Entypo+/arrow-bold-right.svg" height="45px" width="65px"/>TD4</a></h1>
-    <h1><a href="TD5/"><img src="./Entypo+/Entypo+/arrow-bold-right.svg" height="45px" width="65px"/>TD5</a></h1>
-  	<h1><a href="TD6/"><img src="./Entypo+/Entypo+/arrow-bold-right.svg" height="45px" width="65px"/>TD6</a></h1>
-		<h1><a href="TD7/"><img src="./Entypo+/Entypo+/arrow-bold-right.svg" height="45px" width="65px"/>TD7</a></h1>
-
+		
+		<?php
+			$fichier = "fichier.xml";
+			$xml = simplexml_load_file($fichier);
+			print_r($xml);
+		?>
+		
+		
+    <a href="../index.php"><img src="../Entypo+/Entypo+/arrow-bold-left.svg" height="25px" width="45px"/>Retour index</a>   
+   
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
