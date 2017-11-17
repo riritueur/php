1a375dfeec9caa198ddd1eba57ae792bb66de0f5
Richard Peres <riritueur@gmail.com>
2017-11-17 14:47:12 +0100

    td6 final

--- TD6/connect.php
+++ TD6/connect.php
@@ -2,7 +2,7 @@
   session_start();
 		 
   setcookie("login",$_POST['login'],time()+30); 
-setcookie("mdp",$_POST['mdp'],time()+30); 
+  setcookie("mdp",$_POST['mdp'],time()+30); 
 
   header('Location: login.php');
   exit();
--- TD6/mailing.php
+++ TD6/mailing.php
@@ -1,16 +1,16 @@
 <?php
-		 session_start();
-		 
+      
+  session_start();
 
-     $to      = $_POST['email'];
-     $subject = 'Mot de passe oublié';
-     $message = 'Votre mot de passe est '. $_COOKIE['mdp'];
-     $headers = 'From: donotreply@floda' . "\r\n" .
-     'Reply-To: donotreply@floda' . "\r\n" .
-     'X-Mailer: PHP/';
+  $to      = $_POST['email'];
+  $subject = 'Mot de passe oublié';
+  $message = 'Votre mot de passe est '. $_COOKIE['mdp'];
+  $headers = 'From: donotreply@floda.com' . "\r\n" .
+  'Reply-To: donotreply@floda.com' . "\r\n" .
+  'X-Mailer: PHP/';
 
-     mail($to, $subject, $message, $headers);
+  mail($to, $subject, $message, $headers);
 
   header('Location: index.php');
   exit();
- ?>
+?>
